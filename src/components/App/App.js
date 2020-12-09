d4144a1a924547400fd22e96a771c4d6c2008206
sydbthompson <70600089+sydbthompson@users.noreply.github.com>
2020-12-08 21:27:30 -0800

    Add files via upload

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+import Title from '../Title/Title';
+import POPOSList from '../POPOSList/POPOSList';
+import Footer from '../Footer';
+import { HashRouter as Router, Route } from 'react-router-dom'
+import About from '../About/About'
+import POPOSDetails from '../POPOSDetails/POPOSDetails'
+
+function App() {
+  return (
+    <Router>
+
+    <div className="App">
+      <Title />
+      <Route exact path="/" component={POPOSList}/>
+      <Route path="/about" component={About}/>
+      <Route path="/details/:id" component={POPOSDetails} />
+      <POPOSList />
+      <Footer />
+    </div>
+
+  </Router>
+  );
+}
+
+export default App;
